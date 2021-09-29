437fafbbd2b6d9e1752e7d25349855627942df35
Andrey <zabuzamomochi237@gmail.com>
2021-09-29 20:34:31 -0300

    Mãe

--- modelo.py
+++ modelo.py
@@ -1,48 +1,35 @@
-class Filme:
-    def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
+class Programa:                       ## Mãe
+    def __init__(self, nome, ano):
+        self._nome = nome.title()     ## Protegido
         self.ano = ano
-        self.duracao = duracao
-        self.__likes = 0
-
-    
+        self._likes = 0
 
     @property
     def nome(self):
-        return self.__nome
+        return self._nome
 
     @nome.setter
     def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
+        self._nome = novo_nome.title()
 
     @property
     def likes(self):
-        return self.__likes
+        return self._likes
     
     def darLike(self):
         self.likes +=1
 
-class Serie:
+
+class Filme(Programa): ## Declaração de filho
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano) ## 'super()' chama a classe mãe
+        self.duracao = duracao
+        
+
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temopradas = temporadas
-        self.__likes = 0
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_likes(self):
-        self.likes +=1
 
 
 vingadores = Filme('vingadores', 2019, 160)
